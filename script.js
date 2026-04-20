c62cbe0a664ac5873198b94c35614f673785681a
tannerwolff <tawolff13@gmail.com>
2026-04-20 12:38:08 -0700

    Finish playRound() function with logic on winner and incrementing score

--- script.js
+++ script.js
@@ -11,12 +11,15 @@ function getComputerChoice() {
   if (compChoice < 0.333) {
     compChoice = "scissors";
     console.log("Computer chose Scissors!");
+    return "scissors";
   } else if (compChoice >= 0.333 && compChoice < 0.666) {
     compChoice = "rock";
     console.log("Computer chose Rock!");
+    return "rock";
   } else {
     compChoice = "paper";
     console.log("Computer chose Paper!");
+    return "paper";
   }
 }
 
@@ -25,21 +28,42 @@ function getHumanChoice() {
   userChoice = userChoice.toLowerCase();
   if (userChoice === "rock") {
     console.log("You Chose Rock!");
+    return "rock";
   } else if (userChoice === "scissors") {
     console.log("You chose Scissors");
+    return "scissors";
   } else if (userChoice === "paper") {
     console.log("You chose Paper!");
+    return "paper";
   } else {
     alert("Please reload the page to try again! Id10+ error");
   }
 }
 
 function playRound(compChoice, humanChoice) {
-  const compSelection = getComputerChoice();
-  const humanSelection = getHumanChoice();
-  if (compChoice === "rock" && humanChoice === "rock") {
-    console.log("Both chose rock! No one wins!");
+  if (compSelection === humanSelection) {
+    console.log("It's a tie!");
+  } else if (compSelection === "rock" && humanSelection === "scissors") {
+    console.log("Rock beats scissors, You lose!");
+    ++compScore;
+  } else if (compSelection === "scissors" && humanSelection === "rock") {
+    console.log("Rock beats scissors, You win!");
+    ++humanScore;
+  } else if (compSelection === "paper" && humanSelection === "Rock") {
+    console.log("Paper beats rock, You Win!");
+    ++humanScore;
+  } else if (compSelection === "rock" && humanSelection === "paper") {
+    console.log("Paper beats rock, You Win!");
+    ++humanScore;
+  } else if (compSelection === "scissors" && humanSelection === "paper") {
+    console.log("Scissors beats paper, You lose!");
+    ++compScore;
+  } else if (compSelection === "paper" && humanSelection === "scissors") {
+    console.log("Scissors beats paper, You Win!");
+    ++humanScore;
   }
 }
+const compSelection = getComputerChoice();
+const humanSelection = getHumanChoice();
 
-playRound();
+playRound(humanSelection, compSelection);
